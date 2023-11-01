9a88abf5589f817c3fce4a9846f325dcb0d7e93f
Yubo Cai <yubo.cai@polytechnique.edu>
2023-11-01 23:59:26 +0100

    improve playEnemy function

--- Game.hs
+++ Game.hs
@@ -49,55 +49,43 @@ generateWeaponHit hitRate gen =
 --------------------------------------------------------------------
 -- Fighting program between hero and enemy
 --------------------------------------------------------------------
--- This function takes an StdGen and returns an infinite list of StdGen values
--- generateStdGenList :: SR.StdGen -> [SR.StdGen]
--- generateStdGenList gen = let (gen1, gen2) = SR.split gen in gen1 : generateStdGenList gen2
 
 -- This function simulates the fight between the hero and the enemy.
 -- It returns a tuple containing the final state of the hero (if alive) and a list of strings representing the battle log.
-fightEnemy :: Hero -> Enemy -> SR.StdGen -> IO (Maybe Hero)
-fightEnemy hero enemy gen
-  | heroIsDead hero = do
-      putStrLn "The hero is dead."
-      return Nothing
-  | enemyIsDead enemy = do
-      putStrLn "The enemy is dead."
-      return (Just hero)
-  | otherwise = do
-      let heroFirst = totalSpeed hero >= speed enemy
-      let (heroHit, gen') = generateWeaponHit (heroHitChance hero enemy) gen
-      let (enemyHit, gen'') = generateWeaponHit (enemyHitChance enemy hero) gen'
-      let enemyAfterHeroAttack = if heroFirst && heroHit
-                                 then deductHealthFromEnemy enemy (heroHitDamage hero enemy)
-                                 else enemy
-      let heroAfterEnemyAttack = if not heroFirst && enemyHit
-                                 then deductHealth hero (enemyHitDamage enemy hero)
-                                 else hero
-      let enemyAfterAttack = if not heroFirst && heroHit
-                             then deductHealthFromEnemy enemyAfterHeroAttack (heroHitDamage hero enemy)
-                             else enemyAfterHeroAttack
-      let heroAfterAttack = if heroFirst && enemyHit
-                            then deductHealth heroAfterEnemyAttack (enemyHitDamage enemy hero)
-                            else heroAfterEnemyAttack
+fightEnemy :: Hero -> Enemy -> SR.StdGen -> Int -> IO (Maybe Hero)
+fightEnemy hero enemy gen roundNumber = do
+  putStrLn $ "------------------------- Round " ++ show roundNumber ++ " -------------------------"
+  let heroFirst = totalSpeed hero >= speed enemy
+  let (heroHit, gen') = generateWeaponHit (heroHitChance hero enemy) gen
+  let (enemyHit, gen'') = generateWeaponHit (enemyHitChance enemy hero) gen'
+  let enemyAfterHeroAttack = if heroFirst && heroHit
+                             then deductHealthFromEnemy enemy (heroHitDamage hero enemy)
+                             else enemy
+  let heroAfterEnemyAttack = if not heroFirst && enemyHit
+                             then deductHealth hero (enemyHitDamage enemy hero)
+                             else hero
+  let enemyAfterAttack = if not heroFirst && heroHit
+                         then deductHealthFromEnemy enemyAfterHeroAttack (heroHitDamage hero enemy)
+                         else enemyAfterHeroAttack
+  let heroAfterAttack = if heroFirst && enemyHit
+                        then deductHealth heroAfterEnemyAttack (enemyHitDamage enemy hero)
+                        else heroAfterEnemyAttack
 
-      putStrLn $ "Hero attacks: " ++ show heroHit
-      putStrLn $ "Enemy attacks: " ++ show enemyHit
-      putStrLn $ "Hero HP: " ++ show (totalHealthPoints heroAfterAttack) ++ ", Enemy HP: " ++ show (healthPoints enemyAfterAttack)
+  putStrLn $ "Hero attacks: " ++ show heroHit
+  putStrLn $ name enemy ++ " attacks: " ++ show enemyHit
+  putStrLn $ "Hero HP: " ++ show (totalHealthPoints heroAfterAttack) ++ ", " ++ name enemy ++ " HP: " ++ show (healthPoints enemyAfterAttack)
 
-      if heroIsDead heroAfterAttack then do
-        putStrLn "The hero is dead."
-        return Nothing
-      else if enemyIsDead enemyAfterAttack then do
-        putStrLn "The enemy is dead."
-        return (Just heroAfterAttack)
-      else fightEnemy heroAfterAttack enemyAfterAttack gen''
-
-      -- Print attack information
-    --   printAttack "Hero" heroHit hero enemy
-    --   printAttack "Enemy" enemyHit hero enemy
-
-      -- Recursive call if both are still alive
-    --   fightEnemy heroAfterAttack heroAfterAttack gen''
+  if heroIsDead heroAfterAttack then do
+    putStrLn "------------------------- Fight Result -------------------------"
+    putStrLn "The hero is dead."
+    putStrLn $ "Total Round Number: " ++ show roundNumber
+    return Nothing
+  else if enemyIsDead enemyAfterAttack then do
+    putStrLn "------------------------- Fight Result -------------------------"
+    putStrLn $ name enemy ++ " is dead."
+    putStrLn $ "Total Round Number: " ++ show roundNumber
+    return (Just heroAfterAttack)
+  else fightEnemy heroAfterAttack enemyAfterAttack gen'' (roundNumber + 1)
 
 -- Helper function to print attack information
 printAttack :: String -> Bool -> Hero -> Enemy -> IO ()
@@ -146,9 +134,10 @@ testGenerateWeaponHit hitRate gen = runTest 100 gen 0
 
 testFightEnemy :: Hero -> Enemy -> SR.StdGen -> IO ()
 testFightEnemy hero enemy gen = do
-  finalHeroMaybe <- fightEnemy hero enemy gen
+  finalHeroMaybe <- fightEnemy hero enemy gen 1
+  -- No need to print the total round number here, as it's already printed in fightEnemy
   case finalHeroMaybe of
-    Nothing -> putStrLn "The hero is dead."
+    Nothing -> putStrLn "False"
     Just finalHero -> putStrLn $ "Final hero: " ++ show finalHero
 
 -- 主函数，运行测试并打印结果
